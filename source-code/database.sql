c6134829147eecd12a264a9f47deece53c3cd4df
Sunkiller123 <161820532+Sunkiller123@users.noreply.github.com>
2024-05-11 12:54:25 +0200

    Update database.sql

--- source-code/database.sql
+++ source-code/database.sql
@@ -16,3 +16,9 @@ ALTER TABLE movement MODIFY COLUMN move_id int NOT NULL AUTO_INCREMENT;
 ALTER TABLE movement MODIFY posX double NOT NULL;
 ALTER TABLE movement MODIFY posY double NOT NULL;
 ALTER TABLE movement MODIFY posZ double NOT NULL;
+
+CREATE TABLE voltage (
+    voltage_id int not null AUTO_INCREMENT,
+    timestamp VARCHAR(255) not null,
+    spaending double not null,
+);
